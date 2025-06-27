367c9eb90e144637f2c521bb96808f4de5e02dda
viaco <254418958@qq.com>
2025-06-27 21:50:58 +0800

    refactor(config): 重构地图配置加载和处理
    - 新增 _convert_json_data 函数，用于将 JSON 数据中的数组转换为 Vector2 类型
    - 改进 load_from_json 函数，使用新的数据转换逻辑
    - 优化 get_size 函数，增加对不同格式数据的处理
    - 调整 map_config.json 文件，将范围值由数组改为对象形式

--- config/MapConfig.gd
+++ config/MapConfig.gd
@@ -8,26 +8,62 @@ var config = {
 				 "x_range": Vector2(-10, 10),  # x轴范围
 				 "z_range": Vector2(-10, 10),  # z轴范围
 				 "height_range": Vector2(-1, 5)  # 高度范围
-			 }
+}
 
 # 从JSON文件加载配置
 func load_from_json(path: String) -> bool:
 	var file = FileAccess.open(path, FileAccess.READ)
-	if not file: return false
+	if not file: 
+		return false
 
 	var json = JSON.new()
-	if json.parse(file.get_as_text()) != OK: return false
+	if json.parse(file.get_as_text()) != OK: 
+		return false
 
 	var data = json.get_data()
 	if data is Dictionary:
-		config = data
+		# 转换关键字段为Vector2
+		config = _convert_json_data(data)
 		return true
 	return false
 
+# 辅助函数：转换JSON数据中的数组为Vector2
+func _convert_json_data(raw_data: Dictionary) -> Dictionary:
+	var converted = {}
+	
+	# 转换所有Vector2类型的字段
+	for key in ["size", "origin", "x_range", "z_range", "height_range"]:
+		if key in raw_data:
+			var value = raw_data[key]
+			if value is Array and value.size() >= 2:
+				converted[key] = Vector2(value[0], value[1])
+			elif value is Dictionary and "x" in value and "y" in value:
+				converted[key] = Vector2(value["x"], value["y"])
+			else:
+				converted[key] = value
+		else:
+			# 保留未转换的字段
+			converted[key] = raw_data.get(key, config.get(key))
+	
+	# 保留其他可能存在的字段
+	for key in raw_data:
+		if not key in converted:
+			converted[key] = raw_data[key]
+	
+	return converted
+
 # 获取地图尺寸
 func get_size() -> Vector2:
-	return config.get("size", Vector2(20, 20))
-
+	var raw_size = config.get("size", Vector2(20, 20))  # 默认使用数组格式
+	
+	# 类型转换处理
+	if raw_size is Vector2:
+		return raw_size
+	elif raw_size is Array and raw_size.size() >= 2:
+		return Vector2(raw_size[0], raw_size[1])
+	else:
+		push_error("Invalid size format in config, using default")
+		return Vector2(20, 20)
 # 获取坐标范围
 func get_range(axis: String) -> Vector2:
 	match axis:
