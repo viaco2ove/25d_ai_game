c6461de36d8eb52b1a901b06189d4f0b53f996e8
viaco <254418958@qq.com>
2025-06-30 22:04:05 +0800

    feat(login): 重构登录界面并优化用户交互
    - 重新设计了登录界面布局，使其更加美观和用户友好
    - 添加了回车键登录功能，提高用户便利性
    - 优化了错误提示显示，使错误信息更清晰易懂
    - 重构了登录逻辑，增加了输入验证，提高系统安全性

--- login_ui.gd
+++ login_ui.gd
@@ -3,30 +3,52 @@ extends Panel
 signal login_success(user_id: int)
 signal register_request
 
-@onready var nickname_input = $VBoxContainer/NicknameInput
-@onready var password_input = $VBoxContainer/PasswordInput
-@onready var error_label = $VBoxContainer/ErrorLabel
+@onready var nickname_input: LineEdit = $VBoxContainer/NicknameInput
+@onready var password_input: LineEdit = $VBoxContainer/PasswordInput
+@onready var error_label: Label = $VBoxContainer/ErrorLabel
 
 var database: Node
 
 func _ready():
+	# 获取数据库节点
 	database = get_node("/root/data/Database")
+
+	# 连接按钮信号
 	$VBoxContainer/HBoxContainer/LoginBtn.pressed.connect(_on_login_btn_pressed)
 	$VBoxContainer/HBoxContainer/RegisterBtn.pressed.connect(_on_register_btn_pressed)
 
-func _on_login_btn_pressed():
-	var nickname = nickname_input.text
-	var password = password_input.text
+	# 回车键登录
+	nickname_input.text_submitted.connect(_on_text_submitted)
+	password_input.text_submitted.connect(_on_text_submitted)
 
-	if nickname.is_empty() or password.is_empty():
-		error_label.text = "昵称和密码不能为空"
+# 处理输入框回车事件
+func _on_text_submitted(_text: String):
+	_on_login_btn_pressed()
+
+func _on_login_btn_pressed():
+	var nickname = nickname_input.text.strip_edges()
+	var password = password_input.text.strip_edges()
+
+	# 输入验证
+	if nickname.is_empty():
+		error_label.text = "昵称不能为空"
+		nickname_input.grab_focus()
 		return
 
+	if password.is_empty():
+		error_label.text = "密码不能为空"
+		password_input.grab_focus()
+		return
+
+	# 调用数据库登录
 	var user_id = database.login_user(nickname, password)
 	if user_id != -1:
 		login_success.emit(user_id)
+		error_label.text = ""  # 清空错误信息
 	else:
 		error_label.text = "登录失败，请检查昵称或密码"
+		password_input.text = ""  # 清空密码框
+		password_input.grab_focus()
 
 func _on_register_btn_pressed():
 	register_request.emit()
--- register_ui.gd
+++ register_ui.gd
@@ -2,39 +2,54 @@ extends Panel
 
 signal register_success(user_id: int)
 
-@onready var nickname_input = $VBoxContainer/NicknameInput
-@onready var password_input = $VBoxContainer/PasswordInput
-@onready var confirm_password_input = $VBoxContainer/ConfirmPasswordInput
-@onready var gender_option = $VBoxContainer/GenderOption
-@onready var error_label = $VBoxContainer/ErrorLabel
+@onready var nickname_input: LineEdit = $VBoxContainer/NicknameInput
+@onready var password_input: LineEdit = $VBoxContainer/PasswordInput
+@onready var confirm_password_input: LineEdit = $VBoxContainer/ConfirmPasswordInput
+@onready var gender_option: OptionButton = $VBoxContainer/GenderOption
+@onready var error_label: Label = $VBoxContainer/ErrorLabel
+@onready var register_btn: Button = $VBoxContainer/RegisterBtn
 
 var database: Node
 
 func _ready():
 	database = get_node("/root/data/Database")
-	$VBoxContainer/RegisterBtn.pressed.connect(_on_register_btn_pressed)
+	register_btn.pressed.connect(_on_register_btn_pressed)
+
+	# 回车键提交
+	nickname_input.text_submitted.connect(_on_text_submitted)
+	password_input.text_submitted.connect(_on_text_submitted)
+	confirm_password_input.text_submitted.connect(_on_text_submitted)
+
+func _on_text_submitted(_text: String):
+	_on_register_btn_pressed()
 
 func _on_register_btn_pressed():
-	var nickname = nickname_input.text
-	var password = password_input.text
-	var confirm_password = confirm_password_input.text
+	var nickname = nickname_input.text.strip_edges()
+	var password = password_input.text.strip_edges()
+	var confirm_password = confirm_password_input.text.strip_edges()
 	var gender = gender_option.get_item_text(gender_option.selected)
 
-	if nickname.is_empty() or password.is_empty():
-		error_label.text = "昵称和密码不能为空"
+	# 输入验证
+	if nickname.length() < 3 or nickname.length() > 20:
+		error_label.text = "昵称长度需在3-20字符之间"
+		nickname_input.grab_focus()
+		return
+
+	if password.length() < 6:
+		error_label.text = "密码长度至少6位"
+		password_input.grab_focus()
 		return
 
 	if password != confirm_password:
 		error_label.text = "两次输入的密码不一致"
+		confirm_password_input.grab_focus()
 		return
 
-	var user_id = database.register_user(
-					  nickname,
-					  password,
-					  gender
-				  )
-
+	# 调用数据库注册
+	var user_id = database.register_user(nickname, password, gender)
 	if user_id != -1:
 		register_success.emit(user_id)
+		error_label.text = ""  # 清空错误信息
 	else:
-		error_label.text = "注册失败，请重试"
+		error_label.text = "注册失败，昵称可能已被使用"
+		nickname_input.grab_focus()
